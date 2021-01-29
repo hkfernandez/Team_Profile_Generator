39558158f115baa6e049b12de068b39a1b5ece59
Hector Fernandez <hkfernandezdev@gmail.com>
2021-01-28 21:07:57 -0600

    manager question added to index.js - inquirer questions posting to terminal - Manager class creating manager object

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 const inquirer = require("inquirer");
+const fs = require('fs')
 const Employee = require("./lib/Employee.js");
 const Manager = require("./lib/Manager.js");
 const Engineer = require("./lib/Engineer.js");
 const Intern = require("./lib/Intern.js");
 
-let Bob = new Employee ("Bob", 123, "email.test");
-console.log(Bob);
+// let Bob = new Employee ("Bob", 123, "email.test");
+// console.log(Bob);
 
 // questions passed into the inquirer
 const managerQuestions = [
       {type: 'input', message: `Enter Manager's Name`, name: 'name'}, 
       {type: 'input', message: `Enter Manager's Employee ID`, name: 'id'},
       {type: 'input', message: `Enter Manager's Email Address`, name: 'email'},
-      {type: 'input', message: 'Usage - how the app is used', name: 'usage'},
-      {type: 'input', message: "Contributors", name: 'contributors'},
-      {type: 'input', message: "Testing Instructions", name: 'testing'},
-      {type: 'input', message: "GitHub Username", name: 'github'},
-      {type: 'input', message: "Email Address", name: 'email'},
-      {type: 'list', message: "License Type", name: 'license', choices: ['MIT', 'Apache', 'GPL', 'BSD_2_Clause', 'BSD_3_Clause', 'BSD_4_Clause']}
+      {type: 'input', message: `Enter Manager's Office Number`, name: 'officeNumber'},
 ];
 
 function init() {
 // inquirer loop 
       inquirer.prompt(managerQuestions)
-      .then ((response) => {
-
+      .then (({name, id, email, officeNumber }) => {
+            let employee = new Manager (name, id, email, officeNumber);
+            console.log(employee);
       })
 }
+
+init();
+
