d8478be4be7ca93c4218a60f9c73a3d86e8f1166
Hector Fernandez <hkfernandezdev@gmail.com>
2021-01-29 17:32:34 -0600

    moved questions to dedicated file, added a Functions file where employee objects are built

--- index.js
+++ index.js
@@ -1,27 +1,39 @@
 const inquirer = require("inquirer");
 const fs = require('fs')
 const Employee = require("./lib/Employee.js");
-const Manager = require("./lib/Manager.js");
+const manager = require("./lib/Manager.js");
 const Engineer = require("./lib/Engineer.js");
 const Intern = require("./lib/Intern.js");
 
-// let Bob = new Employee ("Bob", 123, "email.test");
+// let Bob = new Manager ("Bob", 123, "email.test");
 // console.log(Bob);
 
+console.log(manager);
+console.log(manager.managerQuestions);
+
 // questions passed into the inquirer
-const managerQuestions = [
-      {type: 'input', message: `Enter Manager's Name`, name: 'name'}, 
-      {type: 'input', message: `Enter Manager's Employee ID`, name: 'id'},
-      {type: 'input', message: `Enter Manager's Email Address`, name: 'email'},
-      {type: 'input', message: `Enter Manager's Office Number`, name: 'officeNumber'},
-];
+// const managerQuestions = [
+//       {type: 'input', message: `Enter Manager's Name`, name: 'name'}, 
+//       {type: 'input', message: `Enter Manager's Employee ID`, name: 'id'},
+//       {type: 'input', message: `Enter Manager's Email Address`, name: 'email'},
+//       {type: 'input', message: `Enter Manager's Office Number`, name: 'officeNumber'},
+//       {type: 'input', message: `Would would like to add a team member?`, name: 'newTeamMember'},
+// ];
 
 function init() {
 // inquirer loop 
-      inquirer.prompt(managerQuestions)
-      .then (({name, id, email, officeNumber }) => {
-            let employee = new Manager (name, id, email, officeNumber);
+      inquirer.prompt(manager.managerQuestions)
+      .then (({ name, id, email, officeNumber, newTeamMember }) => {
+            console.log(manager);
+            let employee = new manager.Manager (name, id, email, officeNumber);
             console.log(employee);
+      //       if (newTeamMember === "yes")
+            
+      // })
+      // inquirer.prompt(managerQuestions)
+      // .then (({name, id, email, officeNumber }) => {
+      //       let employee = new Manager (name, id, email, officeNumber);
+            
       })
 }
 
