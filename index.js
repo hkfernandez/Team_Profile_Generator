534298865813cbf79322e8e306ea98e2ebe2c158
Hector Fernandez <hkfernandezdev@gmail.com>
2021-01-30 11:14:13 -0600

    index.js - added pseude code

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
+// MODULES
+// dependencies
 const inguirer = require ('inquirer');
+// custom 
+const html = require ('./Develop/src/page-template.js');
 const questions = require ('./lib/Questions.js');
 const Manager = require ('./lib/Manager.js');
 const Engineer = require ('./lib/Engineer.js');
 const Intern = require ('./lib/Intern.js');
-const inquirer = require('inquirer');
+
+// GLOBAL VARIABLES
 const employeesArrGV = [];
-let employeeTypeGV = ''
 
-
+// FUNCTIONS
 function buildManagerObj () {
       inquirer.prompt (questions.manager)
             .then (({name, id, email, officeNumber, addTeamMemberChoice})=>{
@@ -17,6 +21,20 @@ function buildManagerObj () {
             })
 }
 
+//called by all the buildEmployeeObj functions
+function addNewEmployee () {
+      inquirer.prompt (questions.addEmployee)
+      .then (({addEmployeeChoice})=>{
+            const addEmployee = addEmployeeChoice;
+            if(addEmployee === "Yes") {
+                  askEmployeeType()
+            }else {
+                  writeHtml();
+            }
+      })
+}
+
+// called by addNewEmployee 
 function askEmployeeType () {
       inquirer.prompt (questions.employeeType)
             .then (({employeeTypeChoice})=>{
@@ -47,17 +65,6 @@ function buildInternObj () {
             })
 }
 
-function addNewEmployee () {
-      inquirer.prompt (questions.addEmployee)
-      .then (({addEmployeeChoice})=>{
-            const addEmployee = addEmployeeChoice;
-            if(addEmployee === "Yes") {
-                  askEmployeeType()
-            }else {
-                  writeHtml();
-            }
-      })
-}
 
 function writeHtml () {
 
@@ -69,3 +76,12 @@ function init () {
 
  init ();
 
+/* PSEUDO CODE
+user is prompted to enter a managers information
+manager object is built and passed to global variable
+user prompted to add another emplyee
+user selects type of employee to add
+depending on type of employee chosen to enter user enters information for that employee
+employee object is pushed to global variable
+once user is done enetering employees an HTML file is written based on the global variable array
+*/
