985e7ba8c43a6ffe3d2cd151b17d13fa2d4cf9b2
Hector Fernandez <hkfernandezdev@gmail.com>
2021-01-29 18:10:49 -0600

    Functions.js-manager object pushing to employeesArr while being called from index.js

--- index.js
+++ index.js
@@ -1,41 +1,54 @@
 const inquirer = require("inquirer");
 const fs = require('fs')
 const Employee = require("./lib/Employee.js");
-const manager = require("./lib/Manager.js");
+const Manager = require("./lib/Manager.js");
 const Engineer = require("./lib/Engineer.js");
 const Intern = require("./lib/Intern.js");
+const functions = require("./lib/Functions.js");
+const employeeArr = [];
 
 // let Bob = new Manager ("Bob", 123, "email.test");
 // console.log(Bob);
 
-console.log(manager);
-console.log(manager.managerQuestions);
 
-// questions passed into the inquirer
-// const managerQuestions = [
-//       {type: 'input', message: `Enter Manager's Name`, name: 'name'}, 
-//       {type: 'input', message: `Enter Manager's Employee ID`, name: 'id'},
-//       {type: 'input', message: `Enter Manager's Email Address`, name: 'email'},
-//       {type: 'input', message: `Enter Manager's Office Number`, name: 'officeNumber'},
-//       {type: 'input', message: `Would would like to add a team member?`, name: 'newTeamMember'},
-// ];
 
 function init() {
-// inquirer loop 
-      inquirer.prompt(manager.managerQuestions)
-      .then (({ name, id, email, officeNumber, newTeamMember }) => {
-            console.log(manager);
-            let employee = new manager.Manager (name, id, email, officeNumber);
-            console.log(employee);
-      //       if (newTeamMember === "yes")
-            
-      // })
-      // inquirer.prompt(managerQuestions)
-      // .then (({name, id, email, officeNumber }) => {
-      //       let employee = new Manager (name, id, email, officeNumber);
-            
-      })
+      functions.buildManagerObj();
 }
+
+
+// // inquirer loop 
+//       inquirer.prompt(questions.managerQuestions)
+//       .then (({ name, id, email, officeNumber, addTeamMemberChoice }) => {
+//             let employee = new Manager.Manager (name, id, email, officeNumber);
+//             let newTeamMember = addTeamMemberChoice;
+//             console.log(newTeamMember);
+//             for (newTeamMember === 'Yes') {
+//                   inquirer.prompt([{
+//                         type: 'list', 
+//                         message: `Would you like to add an engineer or an intern?`, 
+//                         name: 'internOrEngineerChoice', 
+//                         choices: ['Intern', 'Engineer']
+//                   }])
+//                   .then (({internOrEngineerChoice}) => {
+//                         let internOrEngineer = internOrEngineerChoice
+//                         if (internOrEngineer === 'Engineer') {
+//                               inquirer.prompt(engineer.engineerQuestions)
+//                               .then (({ name, id, email, github, addTeamMemberChoice }) => {
+//                                     let employee = new Engineer.Engineer (name, id, email, github);
+//                                     newTeamMember = addTeamMemberChoice;
+//                               })
+//                         } else {
+//                                     inquirer.prompt(intern.internQuestions)
+//                               .then (({ name, id, email, school, addTeamMemberChoice }) => {
+//                                     let employee = new Intern.Intern (name, id, email, school);
+//                                     newTeamMember = addTeamMemberChoice;
+//                               })
+//                         }
+//                   })
+//             }
+//       })
+
 
 init();
 
