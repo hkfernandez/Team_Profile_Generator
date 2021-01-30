00ab1de9e0d8338ceb254a2bac4424f9415b2130
Hector Fernandez <hkfernandezdev@gmail.com>
2021-01-29 22:31:54 -0600

    replaced index.js with Functions.js and then changed the name to index.js - Functions.js is now gone - removed addNewEmployee global variable, now just comparing user choice to create a new emplyee inside the fucntions

--- index.js
+++ index.js
@@ -1,54 +1,56 @@
-const inquirer = require("inquirer");
-const fs = require('fs')
-const Employee = require("./lib/Employee.js");
-const Manager = require("./lib/Manager.js");
-const Engineer = require("./lib/Engineer.js");
-const Intern = require("./lib/Intern.js");
-const functions = require("./lib/Functions.js");
-const employeeArr = [];
-
-// let Bob = new Manager ("Bob", 123, "email.test");
-// console.log(Bob);
+const inguirer = require ('inquirer');
+const questions = require ('./lib/Questions.js');
+const Manager = require ('./lib/Manager.js');
+const Engineer = require ('./lib/Engineer.js');
+const Intern = require ('./lib/Intern.js');
+const inquirer = require('inquirer');
+const employeesArrGV = [];
+let employeeTypeGV = ''
 
 
-
-function init() {
-      functions.buildManagerObj();
+function buildManagerObj () {
+      inquirer.prompt (questions.manager)
+            .then (({name, id, email, officeNumber, addTeamMemberChoice})=>{
+                  let employee = new Manager (name, id, email, officeNumber);
+                  employeesArrGV.push(employee);
+                  if(addTeamMemberChoice === "Yes") {
+                        askEmployeeType();
+                  }
+            })
 }
 
+function askEmployeeType () {
+      inquirer.prompt (questions.employeeType)
+            .then (({employeeTypeChoice})=>{
+                  employeeTypeGV = employeeTypeChoice
+                  if( employeeTypeGV === "Engineer") {
+                        buildEngineerObj ();
+                  } else {
+                        buildInternObj ();
+                  }
+            })
+}
 
-// // inquirer loop 
-//       inquirer.prompt(questions.managerQuestions)
-//       .then (({ name, id, email, officeNumber, addTeamMemberChoice }) => {
-//             let employee = new Manager.Manager (name, id, email, officeNumber);
-//             let newTeamMember = addTeamMemberChoice;
-//             console.log(newTeamMember);
-//             for (newTeamMember === 'Yes') {
-//                   inquirer.prompt([{
-//                         type: 'list', 
-//                         message: `Would you like to add an engineer or an intern?`, 
-//                         name: 'internOrEngineerChoice', 
-//                         choices: ['Intern', 'Engineer']
-//                   }])
-//                   .then (({internOrEngineerChoice}) => {
-//                         let internOrEngineer = internOrEngineerChoice
-//                         if (internOrEngineer === 'Engineer') {
-//                               inquirer.prompt(engineer.engineerQuestions)
-//                               .then (({ name, id, email, github, addTeamMemberChoice }) => {
-//                                     let employee = new Engineer.Engineer (name, id, email, github);
-//                                     newTeamMember = addTeamMemberChoice;
-//                               })
-//                         } else {
-//                                     inquirer.prompt(intern.internQuestions)
-//                               .then (({ name, id, email, school, addTeamMemberChoice }) => {
-//                                     let employee = new Intern.Intern (name, id, email, school);
-//                                     newTeamMember = addTeamMemberChoice;
-//                               })
-//                         }
-//                   })
-//             }
-//       })
+function buildEngineerObj () {
+      inquirer.prompt (questions.engineer)
+            .then (({name, id, email, github, addTeamMemberChoice})=>{
+                  let employee = new Engineer (name, id, email, github);
+                  employeesArrGV.push(employee);
+                  if(addTeamMemberChoice === "Yes") {
+                        askEmployeeType();
+                  }
+            })
+}
 
+function buildInternObj () {
+      inquirer.prompt (questions.intern)
+            .then (({name, id, email, school, addTeamMemberChoice})=>{
+                  let employee = new Intern (name, id, email, school);
+                  employeesArrGV.push(employee);
+                  if(addTeamMemberChoice === "Yes") {
+                        askEmployeeType();
+                  }
+            })
+}
 
-init();
-
+buildManagerObj ();
